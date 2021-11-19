b4ba509822fc74a43bccc2c7f987d4d61d157be6
emil <devbyemil@t-online.de>
2021-11-19 21:58:20 +0100

    finish nav + nav homeScreen version

--- src/navigation/Nav.js
+++ src/navigation/Nav.js
@@ -1,29 +1,54 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import "./Nav.scss";
 
 const Nav = (props) => {
+    const location = useLocation();
+    const isHomeMode = location.pathname === "/";
+
     return (
-        <nav className="nav">
-            <Link className="nav__start" to="/">
-                <img
-                    src="../assets/vfs_logo.png"
-                    alt="Logo"
-                    className="nav__logo"
-                />
-            </Link>
-            <div className="nav__links">
-                <Link className="nav__links__link" to="/exercises">
+        <nav className={isHomeMode ? "navHome" : "nav"}>
+            {!isHomeMode && (
+                <Link className="nav__start" to="/">
+                    <img
+                        src="../assets/vfs_logo.png"
+                        alt="Logo"
+                        className="nav__logo"
+                    />
+                </Link>
+            )}
+            <div className={isHomeMode ? "navHome__links" : "nav__links"}>
+                <Link
+                    className={
+                        isHomeMode ? "navHome__links__link" : "nav__links__link"
+                    }
+                    to="/exercises"
+                >
                     Übungen
                 </Link>
-                <Link className="nav__links__link" to="/events">
+                <Link
+                    className={
+                        isHomeMode ? "navHome__links__link" : "nav__links__link"
+                    }
+                    to="/events"
+                >
                     Termine
                 </Link>
-                <Link className="nav__links__link" to="/more">
+                <Link
+                    className={
+                        isHomeMode ? "navHome__links__link" : "nav__links__link"
+                    }
+                    to="/more"
+                >
                     Mehr
                 </Link>
-                <Link className="nav__links__link" to="/about">
+                <Link
+                    className={
+                        isHomeMode ? "navHome__links__link" : "nav__links__link"
+                    }
+                    to="/about"
+                >
                     Über
                 </Link>
             </div>
